84517430ed301b08a268bf52371b6eb08cadfc39
rpantone-rain <41448075+rpantone-rain@users.noreply.github.com>
2020-06-26 17:46:59 -0700

    Update activationsparsity.py

--- sparselinear/activationsparsity.py
+++ sparselinear/activationsparsity.py
@@ -3,18 +3,20 @@ import torch
 import torch.nn as nn
 
 class ActivationSparsity(nn.Module):
-    """Applies activation sparsity using k-winners strategy to the input
+    """Applies activation sparsity using k-winners strategy to the input.
+    See here: https://arxiv.org/abs/1903.11257
 
     Args:
         in_features (int): size of each input sample
         k (int): The K in K-Winners strategy
         alpha (float): The constant used in updating duty-cycle
         beta (float): Boosting factor for neurons not activated in the previous duty cycle
+        act_sparsity (float): Proportion of innactive units
     Shape:
         Input: (N, in_features) 
         Output: (N, in_features)
     Examples:
-        >>> input = torch.randn(3,10)
+        >>> input = torch.randn(3, 10)
         >>> x = asy.ActivationSparsity(10)
         >>> output = x(input) 
     """
@@ -22,11 +24,11 @@ class ActivationSparsity(nn.Module):
         super(ActivationSparsity, self).__init__()
         self.alpha = alpha
         self.beta = beta
-        self.prev_duty_cycle=None
+        self.prev_duty_cycle = None
         self.act_sparsity = act_sparsity
-        self.k=None
+        self.k = None
     
-    def updateDC(self,inputs, prev_duty_cycle):
+    def updateDC(self, inputs, prev_duty_cycle):
         """Function to calculate the activation sparsity using the k-winners strategy. This is
             to be used as an activation function by the sparselinear layer. 
 
@@ -36,12 +38,12 @@ class ActivationSparsity(nn.Module):
             alpha (float, optional): [Contribution factor of top indices to the duty cycle]. Defaults to 0.1.
             beta (float, optional): [Boost coefficient for units in a layer]. Defaults to 0 i.e. No boosting. 
         """
-        duty_cycle = (1-self.alpha)*self.prev_duty_cycle + self.alpha*inputs
+        duty_cycle = (1 - self.alpha) * self.prev_duty_cycle + self.alpha * inputs
         return duty_cycle
 
     def forward(self, inputs):
-        out_shape=list(inputs.shape)
-        inputs = inputs.reshape(inputs.shape[0],-1)
+        out_shape = list(inputs.shape)
+        inputs = inputs.reshape(inputs.shape[0], -1)
 
         if self.prev_duty_cycle is None:
             self.prev_duty_cycle = torch.zeros(inputs.shape[-1])
